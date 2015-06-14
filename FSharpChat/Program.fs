fa87bc98a6fe76884c06a1f80afa9d536e64634d
Badmoonz <kozlov.mipt@gmail.com>
2015-06-14 13:12:35 +0300

    Add handshaking between users

--- FSharpChat/Program.fs
+++ FSharpChat/Program.fs
@@ -1,5 +1,6 @@
 ﻿module ChatClient
 open System
+open System.Collections.Generic;
 open System.ServiceModel
 open Microsoft.FSharp.Reflection 
 open System.Reflection 
@@ -27,11 +28,15 @@ type Message =
     | JoinChat of JoinMessage
     | LeaveChat of LeaveMessage
     | ChatMessage of MessageItem
+    | HandShake of HandShakeMessage
     static member KnownTypes() = KnownTypes<Message>()
 
 and 
     [<DataContract>]
-    JoinMessage = { [<DataMember>] mutable Name : string}
+    JoinMessage = { [<DataMember>] mutable Name : string;}
+and 
+    [<DataContract>]
+    HandShakeMessage = { [<DataMember>] mutable Name : string; [<DataMember>] mutable TargetName : string; } //[<DataMember>] mutable NickName : string; [<DataMember>] mutable Avatar : string[]
 and
     [<DataContract>]
     LeaveMessage = { [<DataMember>] mutable Name : string}
@@ -53,8 +58,10 @@ type ChatClient(agent : Agent<_>) =
     interface  IChatService with
         member __.Command(msg) = agent.Post msg
 
-let filterMessage name = function
-    | JoinChat { Name = n;  } when n <> name -> true 
+let filterMessage name msg = 
+    match msg with
+    | JoinChat { Name = n; } when n <> name -> true 
+    | HandShake { TargetName = tn } when tn = name -> true 
     | LeaveChat { Name = n; } when n <> name -> true 
     | ChatMessage { Name = n; } when n <> name -> true 
     | _ -> false
@@ -90,9 +97,13 @@ let main argv =
     let philosopher name chatRoom =
         Agent<_>.Start(fun inbox ->
             let channel = createChanel inbox chatRoom
+            let joinAction() = channel.Command <| JoinChat { Name = name;}
             do 
                 channel.Open()
-                channel.Command <| JoinChat { Name = name}
+                channel.GetProperty<IOnlineStatus>().Online.Add(fun _ -> joinAction())
+                joinAction()
+            let online = List<string>();
+
             let rec loop () =
                 async {
                     let! msg' = inbox.TryReceive(500)
@@ -101,24 +112,35 @@ let main argv =
                         if filterMessage name msg then
                             match msg with
                                 | JoinChat { Name = n} ->  
-                                        channel.Command <| ChatMessage { Name = name; Message = sprintf "from %s hi ,%s " name n };
+                                    if (not << online.Contains) n then
+                                        channel.Command <| HandShake { Name = name; TargetName = n;}
+                                    channel.Command <| ChatMessage { Name = name; Message = sprintf "from %s hi ,%s " name n };
+                                | HandShake { Name = n} ->
+                                    if (not << online.Contains) n then
+                                       online.Add n                                  
                                 | LeaveChat {Name = n} -> 
-                                        channel.Command <| ChatMessage { Name = name; Message = sprintf "from %s bye ,%s " name n };
+                                    online.Remove n |> ignore
+                                    consoleActor.Post <| sprintf "[channel : %A][%s]  %s is leaver.people left %A" chatRoom  name n online
+//                                    channel.Command <| ChatMessage { Name = name; Message = sprintf "from %s bye ,%s " name n };
                                 | ChatMessage {Name = n; Message = msg} ->
-                                        consoleActor.Post <| sprintf "[channel : %A][%s] got from [%s] : %s" chatRoom  name n msg
-                    | _ -> ()
-                    if name.EndsWith("2") &&  (rnd.Next(0,100) < 10)
-                    then
-                        consoleActor.Post <| sprintf "%s leaving" name
-                        channel.Command <| LeaveChat {Name = name}                  
+                                    consoleActor.Post <| sprintf "[channel : %A][%s] got from [%s] : %s" chatRoom  name n msg
+                    | _ -> ()            
                     return! loop()
                 }
             loop ())
 
-    philosopher "a1" Common |> ignore
-    philosopher "a2" Common |> ignore
+    let a1 = philosopher "a1" Common 
+    let a2 = philosopher "a2" Common 
+    let a3 = philosopher "a3" Common 
 //    philosopher "a3" (Private "azaza") |> ignore
 //    philosopher "a4" (Private "azaza") |> ignore
 
+//    Threading.Thread.Sleep(4000)
+//    a1.Post <| LeaveChat {Name = "a4"}
+//    a2.Post <| LeaveChat {Name = "a4"}
+//    let a5 = philosopher "a5" Common
+//    let a6 = philosopher "a6" Common
+//    Threading.Thread.Sleep(2000)
+//    a6.Post <| LeaveChat {Name = "a4"}
     Console.ReadLine() |> ignore
     0 // возвращение целочисленного кода выхода
